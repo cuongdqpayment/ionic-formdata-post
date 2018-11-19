eab6306ea3da3aec5e3325e67ff64f54f5fd213c
Doan Quoc Cuong <cuong.dq@mobifone.vn>
2018-11-19 16:52:25 +0700

    cuongdq push by sh

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ server.post('/file_upload',(req,res,next)=>{
 server.get('*', function(req, res){
   //gui trang thai bao noi dung tra ve
   res.writeHead(404, {'Content-Type': 'text/html; charset=utf-8' });
-  res.end('<p>Đừng tìm kiếm vô ích. Đố mầy hack tau đấy!</p>');
+  res.end('<h1>Đừng tìm kiếm vô ích. Đố mầy hack tau đấy!</h1>');
 });
 
 //-----------------------------------------------------
